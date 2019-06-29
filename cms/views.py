61fee71f48563452722e2d85c9499424e89b71af
Azeem Khan <azeemkhan@Azeems-MacBook-Air.local>
2019-06-29 21:32:59 +0530

    Init

--- cms/views.py
+++ cms/views.py
@@ -23,14 +23,14 @@ def cms_home(request):
 
         all_hawker = Hawker.objects.all()
 
-        collect_month_amount = Invoice.objects.filter(
-            i_status='Paid', i_month=month_name).aggregate(Sum('i_paid'))
-        collect_annual_amount = Invoice.objects.filter(
-            i_status='Paid').aggregate(Sum('i_paid'))
+        # collect_month_amount = Invoice.objects.filter(
+        #     i_status='Paid', i_month=month_name).aggregate(Sum('i_paid'))
+        # collect_annual_amount = Invoice.objects.filter(
+        #     i_status='Paid').aggregate(Sum('i_paid'))
 
-        collection_chart = Invoice.objects.filter(
-            i_status='Paid').annotate(Sum('i_paid'))
-        return render(request, 'cms/dashboard.html', {'all_customer': all_customer,  'collect_amount': collect_month_amount, 'collect_total_amount': collect_annual_amount, 'collection_chart': collection_chart, 'all_hawker': all_hawker, 'username': username})
+        # collection_chart = Invoice.objects.filter(
+        #     i_status='Paid').annotate(Sum('i_paid'))
+        return render(request, 'cms/dashboard.html', {'all_customer': all_customer, 'all_hawker': all_hawker, 'username': username})
     
     else:
         return redirect('login')
